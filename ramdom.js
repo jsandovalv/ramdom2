893ff73eebc1adb64e762cf3508494a6c7c17a1f
jorge sandoval <sandoval.jorge.v@gmail.com>
2015-07-02 22:34:53 -0400

    version 1 commit

---
+++ ramdom.js
@@ -0,0 +1,9 @@
+//Math.ramdom devuelve un numero aleatorio entre 0 y 1
+var numero = Math.ramdom();
+
+if (numero <= 0.5){
+  console.log('\n' + numero +' MENOR que 0,5 \n');
+}
+else {
+  console.log('\n' + numero +' MAYOR que 0,5 \n');
+}
